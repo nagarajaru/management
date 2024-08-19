cac506846a7180744f4f552e1ea2e51e89b4472c
nagarajaru <nagarajarun64945@gmail.com>
2024-08-19 10:39:48 -0700

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cartRouter=require('./routes/cartRoutes');
 const app=express();
 
 app.use(cors({
-    origin:'https://system01.netlify.app/http://localhost:5173',
+    origin:'https://system01.netlify.app',
     credentials:true
 }));
 
