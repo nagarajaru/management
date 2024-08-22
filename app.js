122598491d47a1929c21973bbb451f05bd935ecb
nagarajaru <nagarajarun64945@gmail.com>
2024-08-22 15:40:06 -0700

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cartRouter=require('./routes/cartRoutes');
 const app=express();
 
 app.use(cors({
-    origin:['https://system02.netlify.app','http://localhost:5173'],
+    origin:'https://system02.netlify.app',
     credentials:true
 }));
 
